d2e64b9f307cc13d014302e8e75c785896d9eb2a
KyleXD MC <165631517+KyleXD-MC@users.noreply.github.com>
2024-04-02 22:39:05 +0500

    Create SCS-Games.lua

---
+++ SCS-Games.lua
@@ -0,0 +1,4 @@
+---------------------------------------------|
+-- SCS Games = SolarClientSupported Games -- |
+---------------------------------------------|
+return(function(a,e,we,r,dj,du,wi)local a=1)local U={setmetatable,"bBD#BDB$RDT4rtgbdtbgR$TBGDTedtbebtf$ERFBRECEtgvxedxtfcecbgt"}
